9353fafabadab1edf30c432bc697e0e9123faf23
Zulaikha1207 <zulaikhageer@gmail.com>
2023-04-30 18:17:53 +0200

    load data script added

---
+++ src/load_data.py
@@ -0,0 +1,24 @@
+import argparse
+import numpy as np
+import pandas as pd
+import yaml
+from typing import Text
+
+
+def load_data(config_path: Text) -> None:
+    with open("params.yaml") as config_file:
+        config = yaml.safe_load(config_file)
+    
+    print('Loading data..')
+    dataset = pd.read_parquet(config['data']['path_data'])
+    print(dataset.head())
+    print('Data load complete!')
+
+
+#to run from CLI use a constructer that allows to parse config file as an argument to the data_load function
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config', dest='config', required=True)
+    args = args_parser.parse_args()
+
+    load_data(config_path=args.config)
