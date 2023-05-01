e7073f71581d745b738465546132a058d28c8f90
Zulaikha1207 <zulaikhageer@gmail.com>
2023-05-01 09:37:02 +0200

    training xgboost model

--- src/train.py
+++ src/train.py
@@ -3,14 +3,19 @@ import numpy as np
 import pandas as pd
 import yaml
 from typing import Text
+import math
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Lasso
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
 
+import sys
+sys.path.insert(0, './src/train')
 
-def train(config_path: Text) -> None:
+from train import *
+
+def train_model(config_path: Text) -> None:
     with open("params.yaml") as config_file:
         config = yaml.safe_load(config_file)
     
@@ -29,16 +34,21 @@ def train(config_path: Text) -> None:
     X_train = dv.fit_transform(train_dicts)
     y_train = df[config['train']['target_column']].values
 
-
-
-    print(y_train.dtype)
-    lr = LinearRegression()
-    lr.fit(X_train, y_train)
-
-    y_pred = lr.predict(X_train)
-
-    rmse = mean_squared_error(y_train, y_pred, squared=False)
-    print(rmse)
+    print('Get estimator name')
+    estimator_name = config['model']['estimator_name']
+    
+    print('Fitting model..')
+    model = training(input_features=X_train,
+                 target=y_train,
+                 estimator_name=estimator_name,
+                 param_grid= config['model']['estimators'][estimator_name]['param_grid'],
+                 cv=config['model']['cv'])
+    
+    print(f'Best RMSE score: {model.best_score_}')
+    
+    print('Saving model..')
+    #model_path= config["model"]["model_path"]
+    #joblib.dump(model, model_path)
 
 
 #to run from CLI use a constructer that allows to parse config file as an argument to the data_load function
@@ -47,4 +57,4 @@ if __name__ == '__main__':
     args_parser.add_argument('--config', dest='config', required=True)
     args = args_parser.parse_args()
 
-    train(config_path=args.config)
+    train_model(config_path=args.config)
